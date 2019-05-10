23800064eee94ebfa62a882da3818632271c9896
Jessie Zhang <zhang_jessie2017@outlook.com>
2019-05-10 12:40:05 -0700

    Updates

---
+++ precache-manifest.1cd640f5522f5d03303d265e3ab5c98a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8af61e7e38d2fbeccdd9859c353bfa",
+    "url": "/new_site/index.html"
+  },
+  {
+    "revision": "85eb61ee3cc6358360bd",
+    "url": "/new_site/static/css/main.a33be6d8.chunk.css"
+  },
+  {
+    "revision": "37834093d7924cc112d4",
+    "url": "/new_site/static/js/2.a2b3ba63.chunk.js"
+  },
+  {
+    "revision": "85eb61ee3cc6358360bd",
+    "url": "/new_site/static/js/main.11a1538b.chunk.js"
+  },
+  {
+    "revision": "242e2fab869873f53ed4",
+    "url": "/new_site/static/js/runtime~main.3f5a3bf3.js"
+  },
+  {
+    "revision": "90af8ae10d13bce6b9281b94e436235f",
+    "url": "/new_site/static/media/about4.90af8ae1.png"
+  },
+  {
+    "revision": "14bfc59239e8e1cc909e80c3328f3943",
+    "url": "/new_site/static/media/computer.14bfc592.jpeg"
+  },
+  {
+    "revision": "9b636180c73507a9ab62ae5fcad928e5",
+    "url": "/new_site/static/media/project1.9b636180.png"
+  },
+  {
+    "revision": "ec64866f86bf54c0bcd8f36ed0e50d99",
+    "url": "/new_site/static/media/project2.ec64866f.png"
+  },
+  {
+    "revision": "b0a8bee31aad6328ee17eceea1193d92",
+    "url": "/new_site/static/media/project3.b0a8bee3.png"
+  },
+  {
+    "revision": "70dfe29a8c141ea35e421e474945321c",
+    "url": "/new_site/static/media/project4.70dfe29a.png"
+  },
+  {
+    "revision": "86f07f2bbddd741ca4f2bf8096a5f588",
+    "url": "/new_site/static/media/project5.86f07f2b.png"
+  },
+  {
+    "revision": "f054c2529509d42709a5b1c29e11ab2d",
+    "url": "/new_site/static/media/project6.f054c252.png"
+  }
+]);
